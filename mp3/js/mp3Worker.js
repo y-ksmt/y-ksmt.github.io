686d9db05b2b406393c3248514bd9f3224bff20f
y-ksmt <y_ksmt@outlook.jp>
2017-08-31 18:03:12 +0900

    commit console log

--- mp3/js/mp3Worker.js
+++ mp3/js/mp3Worker.js
@@ -3,8 +3,10 @@ importScripts('libmp3lame.min.js');
 var mp3codec;
 
 self.onmessage = function(e) {
+	console.log("--self.onmessage start");
 	switch (e.data.cmd) {
 	case 'init':
+		console.log("----self.onmessage init start");
 		if (!e.data.config) {
 			e.data.config = { };
 		}
@@ -26,16 +28,27 @@ self.onmessage = function(e) {
 			'Output Samplate: '+ Lame.get_in_samplerate(mp3codec) + ' / ',
 			'Bitlate :' +Lame.get_bitrate(mp3codec) + ' / ',
 			'VBR :' + Lame.get_VBR(mp3codec));
+		console.log("----self.onmessage init end");
 		break;
 	case 'encode':
+		console.log("----self.onmessage encode start");
+		console.log(mp3codec);
+		console.log(e.data.buf);
+		console.log(e.data.buf);
+		console.log("----self.onmessage encode 1 次でout of memory発生");
 		var mp3data = Lame.encode_buffer_ieee_float(mp3codec, e.data.buf, e.data.buf);
+		console.log("----self.onmessage encode 2");
 		self.postMessage({cmd: 'data', buf: mp3data.data});
+		console.log("----self.onmessage encode end");
 		break;
 	case 'finish':
+		console.log("----self.onmessage finish start");
 		var mp3data = Lame.encode_flush(mp3codec);
 		self.postMessage({cmd: 'end', buf: mp3data.data});
 		Lame.close(mp3codec);
 		mp3codec = null;
+		console.log("----self.onmessage finish end");
 		break;
 	}
+	console.log("--self.onmessage end");
 };
--- mp3/recordmp3.js
+++ mp3/recordmp3.js
@@ -20,7 +20,7 @@
       }
     });
     var recording = false,
-      currCallback;
+    currCallback;
 
     this.node.onaudioprocess = function(e){
       if (!recording) return;
@@ -31,7 +31,7 @@
       worker.postMessage({
         command: 'record',
         buffer: buffer
-      });
+			});
     }
 
     this.configure = function(cfg){
@@ -39,24 +39,29 @@
         if (cfg.hasOwnProperty(prop)){
           config[prop] = cfg[prop];
         }
-      }
+			}
+			console.log("--this.configure");
     }
 
     this.record = function(){
-      recording = true;
+			recording = true;
+			console.log("--this.record");
     }
 
     this.stop = function(){
-      recording = false;
+			recording = false;
+			console.log("--this.stop");
     }
 
     this.clear = function(){
-      worker.postMessage({ command: 'clear' });
+			worker.postMessage({ command: 'clear' });
+			console.log("--this.clear");
     }
 
     this.getBuffer = function(cb) {
       currCallback = cb || config.callback;
-      worker.postMessage({ command: 'getBuffer' })
+			worker.postMessage({ command: 'getBuffer' })
+			console.log("--this.getBuffer");
     }
 
     this.exportWAV = function(cb, type){
@@ -66,138 +71,151 @@
       worker.postMessage({
         command: 'exportWAV',
         type: type
-      });
-    }
-
-	//Mp3 conversion
-    worker.onmessage = function(e){
-      var blob = e.data;
-	  //console.log("the blob " +  blob + " " + blob.size + " " + blob.type);
-
-	  var arrayBuffer;
-	  var fileReader = new FileReader();
-
-	  fileReader.onload = function(){
-		arrayBuffer = this.result;
-		var buffer = new Uint8Array(arrayBuffer),
-        data = parseWav(buffer);
-
-        console.log(data);
-		console.log("Converting to Mp3");
-		log.innerHTML += "\n" + "Converting to Mp3";
-
-        encoderWorker.postMessage({ cmd: 'init', config:{
-            mode : 3,
-			channels:1,
-			samplerate: data.sampleRate,
-			bitrate: data.bitsPerSample
-        }});
-
-        encoderWorker.postMessage({ cmd: 'encode', buf: Uint8ArrayToFloat32Array(data.samples) });
-        encoderWorker.postMessage({ cmd: 'finish'});
-        encoderWorker.onmessage = function(e) {
-            if (e.data.cmd == 'data') {
-
-				console.log("Done converting to Mp3");
-				log.innerHTML += "\n" + "Done converting to Mp3";
-
-				/*var audio = new Audio();
-				audio.src = 'data:audio/mp3;base64,'+encode64(e.data.buf);
-				audio.play();*/
-
-				//console.log ("The Mp3 data " + e.data.buf);
-
-				var mp3Blob = new Blob([new Uint8Array(e.data.buf)], {type: 'audio/mp3'});
-				uploadAudio(mp3Blob);
-
-				var url = 'data:audio/mp3;base64,'+encode64(e.data.buf);
-				var li = document.createElement('li');
-				var au = document.createElement('audio');
-				var hf = document.createElement('a');
-
-				au.controls = true;
-				au.src = url;
-				hf.href = url;
-				hf.download = 'audio_recording_' + new Date().getTime() + '.mp3';
-				hf.innerHTML = hf.download;
-				li.appendChild(au);
-				li.appendChild(hf);
-				recordingslist.appendChild(li);
-
-            }
-        };
-	  };
-
-	  fileReader.readAsArrayBuffer(blob);
-
-      currCallback(blob);
-    }
-
-
-	function encode64(buffer) {
-		var binary = '',
-			bytes = new Uint8Array( buffer ),
-			len = bytes.byteLength;
-
-		for (var i = 0; i < len; i++) {
-			binary += String.fromCharCode( bytes[ i ] );
-		}
-		return window.btoa( binary );
-	}
-
-	function parseWav(wav) {
-		function readInt(i, bytes) {
-			var ret = 0,
-				shft = 0;
-
-			while (bytes) {
-				ret += wav[i] << shft;
-				shft += 8;
-				i++;
-				bytes--;
-			}
-			return ret;
-		}
-		if (readInt(20, 2) != 1) throw 'Invalid compression code, not PCM';
-		if (readInt(22, 2) != 1) throw 'Invalid number of channels, not 1';
-		return {
-			sampleRate: readInt(24, 4),
-			bitsPerSample: readInt(34, 2),
-			samples: wav.subarray(44)
-		};
-	}
-
-	function Uint8ArrayToFloat32Array(u8a){
-		var f32Buffer = new Float32Array(u8a.length);
-		for (var i = 0; i < u8a.length; i++) {
-			var value = u8a[i<<1] + (u8a[(i<<1)+1]<<8);
-			if (value >= 0x8000) value |= ~0x7FFF;
-			f32Buffer[i] = value / 0x8000;
-		}
-		return f32Buffer;
-	}
-
-	function uploadAudio(mp3Data){
-		var reader = new FileReader();
-		reader.onload = function(event){
-			var fd = new FormData();
-			var mp3Name = encodeURIComponent('audio_recording_' + new Date().getTime() + '.mp3');
-			console.log("mp3name = " + mp3Name);
-			fd.append('fname', mp3Name);
-			fd.append('data', event.target.result);
-			$.ajax({
-				type: 'POST',
-				url: 'upload.php',
-				data: fd,
-				processData: false,
-				contentType: false
-			}).done(function(data) {
-				//console.log(data);
-				log.innerHTML += "\n" + data;
 			});
-		};
-		reader.readAsDataURL(mp3Data);
-	}
+			console.log("--this.exportWAV");
+		}
+
+		//Mp3 conversion
+    worker.onmessage = function(e){
+			console.log("--worker.onmessage start");
+      var blob = e.data;
+	  	console.log("the blob " +  blob + " " + blob.size + " " + blob.type);
+
+	  	var arrayBuffer;
+	  	var fileReader = new FileReader();
+
+	  	fileReader.onload = function(){
+				arrayBuffer = this.result;
+				var buffer = new Uint8Array(arrayBuffer),
+						data = parseWav(buffer);
+
+				console.log(data);
+				console.log("Converting to Mp3");
+				log.innerHTML += "\n" + "Converting to Mp3";
+
+				encoderWorker.postMessage({ cmd: 'init', config:{
+					mode : 3,
+					channels:1,
+					samplerate: data.sampleRate,
+					bitrate: data.bitsPerSample
+				}});
+				console.log("--encoderWorker.postMessage:init");
+
+				encoderWorker.postMessage({ cmd: 'encode', buf: Uint8ArrayToFloat32Array(data.samples) });
+				console.log("--encoderWorker.postMessage:encode");
+
+				encoderWorker.postMessage({ cmd: 'finish'});
+				console.log("--encoderWorker.postMessage:finish");
+
+				console.log("--encoderWorker.onmessage start");
+				encoderWorker.onmessage = function(e) {
+					console.log("--encoderWorker.onmessage 1");
+					if (e.data.cmd == 'data') {
+						console.log("Done converting to Mp3");
+						log.innerHTML += "\n" + "Done converting to Mp3";
+
+						/*var audio = new Audio();
+						audio.src = 'data:audio/mp3;base64,'+encode64(e.data.buf);
+						audio.play();*/
+						//console.log ("The Mp3 data " + e.data.buf);
+
+						var mp3Blob = new Blob([new Uint8Array(e.data.buf)], {type: 'audio/mp3'});
+						uploadAudio(mp3Blob);
+
+						var url = 'data:audio/mp3;base64,'+encode64(e.data.buf);
+						var li = document.createElement('li');
+						var au = document.createElement('audio');
+						var hf = document.createElement('a');
+
+						au.controls = true;
+						au.src = url;
+						hf.href = url;
+						hf.download = 'audio_recording_' + new Date().getTime() + '.mp3';
+						hf.innerHTML = hf.download;
+						li.appendChild(au);
+						li.appendChild(hf);
+						recordingslist.appendChild(li);
+					}
+					console.log("--encoderWorker.onmessage 2");
+				};
+				console.log("--encoderWorker.onmessage end");
+	  	};
+
+			console.log("----fileReader.readAsArrayBuffer(blob) start");
+			fileReader.readAsArrayBuffer(blob);
+			console.log("----fileReader.readAsArrayBuffer(blob) end");
+
+			console.log("----currCallback(blob) start");
+			currCallback(blob);
+			console.log("----currCallback(blob) end");
+			
+			console.log("--worker.onmessage end");
+    }
+
+		function encode64(buffer) {
+			var binary = '',
+				bytes = new Uint8Array( buffer ),
+				len = bytes.byteLength;
+
+			for (var i = 0; i < len; i++) {
+				binary += String.fromCharCode( bytes[ i ] );
+			}
+			return window.btoa( binary );
+		}
+
+		function parseWav(wav) {
+			function readInt(i, bytes) {
+				var ret = 0,
+					shft = 0;
+
+				while (bytes) {
+					ret += wav[i] << shft;
+					shft += 8;
+					i++;
+					bytes--;
+				}
+				return ret;
+			}
+			if (readInt(20, 2) != 1) throw 'Invalid compression code, not PCM';
+			if (readInt(22, 2) != 1) throw 'Invalid number of channels, not 1';
+			return {
+				sampleRate: readInt(24, 4),
+				bitsPerSample: readInt(34, 2),
+				samples: wav.subarray(44)
+			};
+		}
+
+		function Uint8ArrayToFloat32Array(u8a){
+			var f32Buffer = new Float32Array(u8a.length);
+			for (var i = 0; i < u8a.length; i++) {
+				var value = u8a[i<<1] + (u8a[(i<<1)+1]<<8);
+				if (value >= 0x8000) value |= ~0x7FFF;
+				f32Buffer[i] = value / 0x8000;
+			}
+			return f32Buffer;
+		}
+
+		function uploadAudio(mp3Data){
+			var reader = new FileReader();
+			reader.onload = function(event){
+				var fd = new FormData();
+				var mp3Name = encodeURIComponent('audio_recording_' + new Date().getTime() + '.mp3');
+				console.log("mp3name = " + mp3Name);
+				fd.append('fname', mp3Name);
+				fd.append('data', event.target.result);
+				$.ajax({
+					type: 'POST',
+					url: 'upload.php',
+					data: fd,
+					processData: false,
+					contentType: false
+				}).done(function(data) {
+					//console.log(data);
+					log.innerHTML += "\n" + data;
+				});
+			};
+			reader.readAsDataURL(mp3Data);
+		}
 
     source.connect(this.node);
     this.node.connect(this.context.destination);    //this should not be necessary
